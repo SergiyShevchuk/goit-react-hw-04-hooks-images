e618f61d9a24b9d1c7c1d69425f2e0aa85c12f60
Sergiy Shevchuk <sirjjjjjjj@gmail.com>
2022-06-01 19:43:35 +0300

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Grid } from 'react-loader-spinner';
+import RingLoader from 'react-spinners/RingLoader';
 import styles from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={styles.loaderWrapper}>
-      <Grid color="#3f51b5" height={200} width={200} />
+      <RingLoader color={'#3f51b5'} />
     </div>
   );
 };
